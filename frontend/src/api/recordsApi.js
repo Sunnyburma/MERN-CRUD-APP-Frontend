7a338bfd16afd74986776d5e4965573e6adb9a77
Sunnyburma <kanishburman111@gmail.com>
2026-06-18 23:21:21 +0530

    to update url on frontend

--- frontend/src/api/recordsApi.js
+++ frontend/src/api/recordsApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://mern-crud-app-h49t.onrender.com', //http://localhost:5000/api
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://mern-crud-app-h49t.onrender.com/api', //http://localhost:5000/api
   headers: {
     'Content-Type': 'application/json'
   }
